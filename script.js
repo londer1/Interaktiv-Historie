8e762686389cb3579d2e0eb6b5f6ccf7c0f0536e
londer1 <148044380+londer1@users.noreply.github.com>
2024-05-03 10:43:41 +0200

    meow

--- script.js
+++ script.js
@@ -3,67 +3,69 @@ const optionsBox = document.getElementById('options');
 
 const story = [
     // StoryIndex 0
-    { text: "You are controlling a person named Lars.", image: "", options: ["Continue"] },
+    { text: "You're guiding Lars through a pivotal day in his life.", image: "", options: ["Continue"] },
     // StoryIndex 1
-    { text: "Lars woke up nervous, feeling a mix of excitement and fear for the first day of school. Should he try to make new friends or stick with his old ones? The decision weighed heavily on him as he stood in front of the mirror, uncertain of which path to choose.", image: ["images/lars-bed.png", "images/lars-mirror.png"], options: ["Make new friends", "Stick with old friends"] },
+    { text: "Lars woke up, feeling nervous and excited for the first day of school. He stood before the mirror, wondering whether to make new friends or stick with the old ones.", image: ["images/lars-bed.png", "images/lars-mirror.png"], options: ["Make new friends", "Stick with old friends"] },
     // StoryIndex 2
-    { text: "Lars decides to make new friends, feeling excited about the opportunity to meet new people and explore new interests. He strikes up conversations with classmates during breaks and ends up joining a group interested in urban exploration and adventure.", image: ["images/lars-newfriends.png"], options: ["Continue"] },
+    { text: "Opting for a change, Lars sets out to make new connections. Engaging in conversations during breaks, he finds himself drawn to a group intrigued by urban exploration and adventure.", image: ["images/lars-newfriends.png"], options: ["Continue"] },
     // StoryIndex 3
-    { text: "Lars chooses to stick with his old friends, feeling more comfortable with familiar faces. They spend the day catching up and reminiscing about past adventures, but Lars can't shake the feeling that something is missing.", image: "images/lars-friends.png", options: ["Continue"] },
+    { text: "Preferring the comfort of familiarity, Lars chooses to stay with his existing friends. They spend the day reminiscing about past experiences, yet Lars senses something left unexplored.", image: "images/lars-friends.png", options: ["Continue"] },
     // StoryIndex 4
-    { text: "Regardless of Lars's choice, he encounters Sara, a good friend from high school. They catch up and share their excitement about the new school year. He gets to talk to new people because they came up to him.", image: "images/lars-sara.png", options: ["Continue"] },
+    { text: "Regardless of Lars's choice, he reunites with Sara, a friend from high school. Their encounter is filled with excitement for the new school year, fostering connections old and new.", image: "images/lars-sara.png", options: ["Continue"] },
     // StoryIndex 5
-    { text: "Peer Pressure\nLars's new friends suggest trying out some joints after school, eager to include him in their activities. Lars feels torn between the desire to fit in and the instinct to stay true to himself. As he hesitates, he notices the expectant looks on his friends' faces, feeling the weight of their expectations pressing down on him.", image: "images/lars-sara-newfriends.png", options: ["Say yes to trying joints after school", "Try to say no in a polite way"] },
+    { text: "Lars's newfound friends suggest trying joints after school, eager to include him. Lars feels torn between the desire to belong and a growing unease, wrestling with conflicting impulses.", image: "images/lars-sara-newfriends.png", options: ["Consider trying joints", "Politely decline"] },
     // StoryIndex 6
-    { text: "The school day is almost done, and Lars's new friends come up again and ask, 'So you come after school?'", image: "images/lars-do-you-come.png", options: ["Continue experimenting with drugs", "Avoid drugs and focus on other activities"] },
+    { text: "As the school day ends, Lars's friends bring up the topic again. 'Are you joining us after school?' they ask eagerly.", image: "images/lars-do-you-come.png", options: ["Continue experimenting", "Suggest an alternative activity"] },
     // StoryIndex 7
-    { text: "Feeling a mix of curiosity and peer pressure, Lars decides to join the group and try the joints. He rationalizes it as a one-time thing, eager to fit in with his new friends.", image: "image7.jpg", options: ["Continue"] },
+    { text: "Torn between curiosity and apprehension, Lars yields to peer pressure, joining his friends in trying the joints. Rationalizing it as a fleeting experience, he ventures into unfamiliar territory, feeling uncertain.", image: "image7.jpg", options: ["Continue"] },
     // StoryIndex 8
-    { text: "Lars declines the offer to try the joints, feeling uncomfortable with the idea of experimenting with drugs. He suggests doing something else instead, like grabbing a snack or exploring the city. The faces of the new faces drop and look disappointed.", image: "image8.jpg", options: ["Continue"] },
+    { text: "Opting for caution, Lars declines the offer, uncomfortable with the idea of drug experimentation. Proposing an alternative activity, he seeks to steer away from risky ventures, though met with disappointment.", image: "images/lars-reject-joints.png", options: ["Continue"] },
     // StoryIndex 9
-    { text: "After trying the joints, Lars finds himself enjoying the sensation and decides to continue experimenting with drugs. He becomes more involved with his new friends and their drug-related activities.", image: "images/lars-smoke.png", options: ["Continue"] },
+    { text: "Exploring drugs, Lars finds himself enticed by the allure of altered sensations, his initial trepidation giving way to immersion in his friends' lifestyle.", image: "images/lars-smoke.png", options: ["Continue"] },
     // StoryIndex 10
-    { text: "By avoiding drugs, Lars focuses on other activities like sports, music, or academic pursuits. He maintains a distance from the group experimenting with drugs, feeling uneasy about their lifestyle choices.", image: "image10.jpg", options: ["Continue"] },
+    { text: "Lars chooses to stay away from drugs and instead focuses on other things. He's careful about who he hangs out with and makes his own choices, being aware of the risks he wants to avoid.", image: "images/lars-reject-joints2.png", options: ["Continue"] },
     // StoryIndex 11
-    { text: "As Lars's drug use escalates, Sara expresses concern for his well-being. She encourages him to seek help and offers her support.", image: "images/sara-help.png", options: ["Accept Sara's help", "Reject Sara's help"] },
+    { text: "Amidst concerns, Sara extends a supportive hand to Lars, expressing worry for his well-being. Offering solace and encouragement, she stands by his side, a beacon of hope in challenging times.", image: "images/sara-help.png", options: ["Accept Sara's help", "Decline Sara's help"] },
     // StoryIndex 12
-    { text: "Recognizing the impact of his choices, Lars accepts Sara's help and decides to overcome his addiction. He opens up to her about his struggles and together, they embark on a journey towards recovery. Their bond deepens, and they confess their feelings for each other, realizing their love was mutual all along. Lars embraces a brighter future with Sara by his side.", image: "image12.jpg", options: ["Continue"] },
+    { text: "Recognizing the gravity of his situation, Lars embraces Sara's support, embarking on a journey towards recovery. Opening up about his struggles, he finds solace in their shared resolve, their bond deepening as they navigate the challenges together. In Sara's steadfast presence, Lars glimpses a brighter future, one infused with hope and renewed purpose.", image: "image12.jpg", options: ["Continue"] },
     // StoryIndex 13
-    { text: "Feeling defensive and ashamed, Lars rejects Sara's help, believing he can handle his addiction on his own. He distances himself from Sara and sinks deeper into addiction, isolating himself from friends and family. Their friendship deteriorates, and Lars's life descends into darkness. Tragedy strikes when Lars's drug use leads to irreversible consequences, leaving Sara devastated.", image: "image13.jpg", options: ["Continue"] },
+    { text: "Faced with vulnerability and pride, Lars rebuffs Sara's offer, convinced he can tackle his demons alone. Drifting further into isolation, he grapples with addiction's relentless grip, his life veering towards darkness as he shuns the support of friends and loved ones. In his solitary descent, Lars faces irreversible consequences, his choices exacting a toll too heavy to bear.", image: "image13.jpg", options: ["Continue"] },
     // StoryIndex 14
     { text: "The End", image: "", options: ["Restart"] },
     // SceneIndex 15
-    { text: "Lars firmly declines the offer to try the joints once again, feeling uneasy about experimenting with drugs. He suggests doing something else instead, hoping to steer the conversation in a different direction. The disappointment on his new friends' faces is palpable.", image: "images/lars-decline.png", options: ["Continue"] },
+    { text: "Lars firmly declines the offer to try the joints once again, feeling uneasy about experimenting with drugs. He suggests doing something else instead, hoping to steer the conversation in a different direction. The disappointment on his new friends' faces is evident.", image: "images/lars-decline.png", options: ["Continue"] },
     // SceneIndex 16
-    { text: "Lars comes home, and sees a 52-year-old, 6-foot-2 tall man. He approaches Lars and grabs him.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars comes home and sees a 52-year-old, 6-foot-2 tall man. He approaches Lars and grabs him.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 17
-    { text: "In the dimly lit foyer of his home, Lars froze as a towering figure emerged from the shadows. Before he could react, the stranger seized him with frightening strength. Despite his desperate struggles, Lars was overpowered, his cries drowned out by the rush of adrenaline. In that moment, his sanctuary became a nightmare, the darkness swallowing him whole. The man takes him in a van and drives away. The man takes him in a dark basement tied down on a chair.", image: "images/lars-abducted.png", options: ["Continue"] },
+    { text: "In the dimly lit foyer of his home, Lars freezes as a towering figure emerges from the shadows. Before he can react, the stranger seizes him with frightening strength. Despite his desperate struggles, Lars is overpowered, his cries drowned out by the rush of adrenaline. In that moment, his sanctuary becomes a nightmare, the darkness swallowing him whole. The man takes him in a van and drives away. The man takes him in a dark basement tied down on a chair.", image: "images/lars-abducted.png", options: ["Continue"] },
     // SceneIndex 18
     { text: "The man goes to the toilet, Lars is stuck, Lars sees a Glock pistol aside from him. Should he take the chance and try to get loose to get the gun, or not?", image: "images/lars-tied.png", options: ["Gun", "Rope", "Wait"] },
     // SceneIndex 19
-    { text: "The man comes out and its a grad reveal that it is his dad.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "The man comes out, revealing himself as Lars's dad.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 20
-    { text: "Lars and his dad lives happily ever after and Sara and Lars become a couple.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars and his dad live happily ever after, and Sara and Lars become a couple.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 21
-    { text: "Lars gets the ropes lose, and ties them down so he can easily get lose later.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars gets the ropes loose and ties them down so he can easily get loose later.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 22
-    { text: "Lars knows a little bit about guns, so he quickly checks if it is loaded. Lars finds out that the gun is empty and have no bullets.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars knows a little about guns, so he quickly checks if it is loaded. Lars finds out that the gun is empty and has no bullets.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 23
     { text: "Should he look for bullets, take the risk to leave, or go back and wait?", image: "images/lars-home.png", options: ["Look for bullets", "Try to escape using the door", "Go back and wait"] },
     // SceneIndex 24
-    { text: "Lars takes the risk and runs out the door. The man comes out of the toilet and screams “Stop!” Lars runs anywas, he does not take that risk.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars takes the risk and runs out the door. The man comes out of the toilet and screams 'Stop!' Lars runs anyway, not taking that risk.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 25
-    { text: "Lars moves away and never gets to see his father again and will most likely never know that was his father trying to do a grand reveal that it is his father.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars moves away and never gets to see his father again and will most likely never know that was his father trying to do a grand reveal.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 26
-    { text: "Lars finds a mag in a drawer. Lars has become a little mentally ill, so he gets the thought, should he wait for the man and shoot or just shoot himself?", image: "images/lars-home.png", options: ["Obviously shoot the man", "Shoot yourself (kys)"] },
+    { text: "Lars finds a magazine in a drawer. Lars has become a little mentally ill, so he gets the thought, should he wait for the man and shoot or just shoot himself?", image: "images/lars-home.png", options: ["Obviously shoot the man", "Shoot yourself (kys)"] },
     // SceneIndex 27
-    { text: "Lars chooses to shot himself (he fucking dies so yeah, story over)", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars chooses to shoot himself (he dies)", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 28
-    { text: "Lars gets ready, and when the man comes out, Lars points the gun against him. The man tries to de-escalate the situation but Lars dont believe it and shoots him 2 times in the head.", image: "images/lars-home.png", options: ["Continue"] },
+    { text: "Lars gets ready, and when the man comes out, Lars points the gun against him. The man tries to de-escalate the situation but Lars doesn't believe it and shoots him twice in the head.", image: "images/lars-home.png", options: ["Continue"] },
     // SceneIndex 29
-    { text: "Lars later finds out that the man was his dad, and that he was doing a grand reveal, so Lars shoots himself.", image: "images/lars-home.png", options: ["Continue"] },
-
+    { text: "Lars later finds out that the man was his dad, and that he was trying to do a grand reveal, so Lars shoots himself.", image: "images/lars-home.png", options: ["Continue"] },
 ];
+
+
+
 
 let storyIndex = 0;
 
@@ -109,17 +111,17 @@ function handleOption(option) {
         storyIndex = 5;
     } else if (storyIndex === 1 && option === 'Stick with old friends') {
         storyIndex = 3;
-    } else if (storyIndex === 5 && option === "Say yes to trying joints after school") {
+    } else if (storyIndex === 5 && option === "Consider trying joints") {
         storyIndex = 7;
     } else if (storyIndex === 7 && option === 'Continue') {
         storyIndex = 6;
-    } else if (storyIndex === 5 && option === "Try to say no in a polite way") {
+    } else if (storyIndex === 5 && option === "Politely decline") {
         storyIndex = 8;
     } else if (storyIndex === 8 && option === 'Continue') {
         storyIndex = 6;
-    } else if (storyIndex === 6 && option === 'Continue experimenting with drugs') {
+    } else if (storyIndex === 6 && option === 'Continue experimenting') {
         storyIndex = 9;
-    } else if (storyIndex === 6 && option === 'Avoid drugs and focus on other activities') {
+    } else if (storyIndex === 6 && option === 'Suggest an alternative activity') {
         storyIndex = 10;
     } else if (storyIndex === 9 && option === 'Continue') {
         storyIndex = 11;
@@ -165,19 +167,11 @@ function handleOption(option) {
         storyIndex = 14;
     } else if (storyIndex === 25 && option === 'Continue') {
         storyIndex = 14;
-
-
     } else if (storyIndex === 14 && option === 'Restart') {
         storyIndex = 0;
-    
-
-
-
-
-
     } else if (storyIndex === 11 && option === "Accept Sara's help") {
         storyIndex = 12;
-    } else if (storyIndex === 11 && option === "Reject Sara's help") {
+    } else if (storyIndex === 11 && option === "Decline Sara's help") {
         storyIndex = 13;
     } else if (storyIndex === 12 && option === 'Continue') {
         storyIndex = 14; // End of the story
@@ -189,5 +183,16 @@ function handleOption(option) {
 }
 
 
+
 // Call displayStory() after setting the initial storyIndex
 displayStory();
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("mousemove", function(e) {
+        const x = e.clientX / window.innerWidth;
+        const y = e.clientY / window.innerHeight;
+
+        // Adjust the background position based on the mouse's position
+        document.body.style.backgroundPosition = `${x * 100}% ${y * 100}%`;
+    });
+});
